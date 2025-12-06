408e66004d53adb34d571a6a890c1e1bf3717e45
Cale Oden Seymour <33235840+caleoseymour@users.noreply.github.com>
2025-12-06 15:33:54 -0800

    Automation
    More changes for the sake of reproducibility; fully  from 'main.R'

--- main.R
+++ main.R
@@ -11,12 +11,19 @@ file_list = c(
 
 ## Output directory
 output_dir = "reports"
+#image_dir = "sanitized-data"
 if (!dir.exists(output_dir)) dir.create(output_dir);
+#if (!dir.exists(image_dir)) dir.create(image_dir);
 
 ## Knit files.
-silence({
+#silence({
 lapply(file_list, function(rmd_file)
 {
 	output_file = file.path(output_dir, tools::file_path_sans_ext(basename(rmd_file)))
-	render(rmd_file, output_file = paste0(output_file, ".html"))
-})})
+	render(rmd_file, output_file = paste0(output_file, ".html"), envir = new.env())
+	
+	## Workaround to make sure the image is saved.
+	#image_file = file.path(image_dir, tools::file_path_sans_ext(basename(rmd_file)))
+	#save.image(file = paste0(image_file, ".Rdata"))
+})
+#})
